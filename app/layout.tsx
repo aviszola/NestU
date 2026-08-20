b5bd58bcf0c5fca476c69807293547c43a21c022
Rameyy00 <zolaziyan2616@gmail.com>
2026-08-20 15:11:01 +0700

    huya

--- app/layout.tsx
+++ app/layout.tsx
@@ -45,6 +45,14 @@ export default function RootLayout({
       lang="en"
       className={`${plusJakartaSans.variable} h-full antialiased`}
     >
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&display=swap"
+        />
+      </head>
       <body className="min-h-full flex flex-col" suppressHydrationWarning>{children}<Toaster position="top-right" richColors={false} /></body>
     </html>
   );
